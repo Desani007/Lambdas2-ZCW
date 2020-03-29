232e347132f7fac5bd018cb66781b26700be00f1
dbond <debon1426@gmail.com>
2020-03-29 03:35:27 -0400

    or maybe its simply in one class

---
+++ src/main/java/Application/Approaches.java
@@ -0,0 +1,39 @@
+package Application;
+
+import java.util.List;
+// local class  is a nested class a class within a class
+public class Approaches {
+
+    public static void printPersonsOlderThan(List<Person> roster, int age) {
+        for (Person p : roster) {
+            if (p.getAge() >= age) {
+                p.printPerson();
+            }
+        }
+    }
+
+
+    public static void printPersonsWithinAgeRange(
+            List<Person> roster, int low, int high) {
+        for (Person p : roster) {
+            if (low <= p.getAge() && p.getAge() < high) {
+                p.printPerson();
+            }
+        }
+
+    }     public static void printPersons(
+                List<Person> roster, CheckPerson tester) {
+            for (Person p : roster) {
+                if (tester.test(p)) {
+                    p.printPerson();
+                }
+            }
+        }
+
+    public interface CheckPerson {
+
+        boolean test(Person p);
+
+
+    }
+}
